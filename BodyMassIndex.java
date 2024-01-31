84d864094e4d916341596e98d0c6df055812262c
AhmetEmirCetin <146943192+AhmetEmirCetin@users.noreply.github.com>
2024-01-31 18:08:21 +0300

    Update Body MassIndex.java

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -8,7 +8,7 @@ public class BodyMassIndex {
         System.out.println("Welcome to body mass index calculation.");
         System.out.print("Please enter your height in meters = ");
         double height = input.nextDouble();
-        System.out.println("Please enter your weight in kilograms as a whole number = ");
+        System.out.print("Please enter your weight in kilograms as a whole number = ");
         int kilogram = input.nextInt();
         //A welcome has been made for the user and some information has been requested.
 
@@ -16,11 +16,11 @@ public class BodyMassIndex {
         //Body mass index has been calculated.
 
         if((18.5<account) && (account<24.9)) {
-            System.out.println("You are at your ideal weight.");
+            System.out.print("You are at your ideal weight.");
         }else if (24.9<account) {
-            System.out.println("You are above your ideal weight.");
+            System.out.print("You are above your ideal weight.");
         }else
-            System.out.println("You are below your ideal weight.");
+            System.out.print("You are below your ideal weight.");
         /*The value entered by the user has been shown to
         the user on the console, indicating which scale it falls into.*/
     }
